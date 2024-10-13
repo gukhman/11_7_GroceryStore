770d1245630d3bbc11ab640d404c015bb1094e81
gukhman <gukhman@gmail.com>
2024-10-13 13:25:02 +0200

    12_9_Реализовал возможность обратиться к галерее и поменять картинку в ItemDescription

--- app/src/main/java/com/example/grocerystore/ItemDescription.kt
+++ app/src/main/java/com/example/grocerystore/ItemDescription.kt
@@ -3,7 +3,6 @@ package com.example.grocerystore
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
